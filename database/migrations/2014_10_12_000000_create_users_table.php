3f675d9da44cc1590139845f192ea87eec19f123
gravity-z <zikolo.sakhe@gmail.com>
2023-01-19 11:57:47 +0200

    user migration done

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,15 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('phone_number');
             $table->string('email')->unique();
+            $table->enum('user_type', ['commuter', 'driver', 'operator', 'organization']);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            //            $table->index('user_type');
             $table->timestamps();
         });
     }
