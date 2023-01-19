bf81c43084b6aee53ef0e7b120d47de92091d572
gravity-z <zikolo.sakhe@gmail.com>
2023-01-19 13:51:09 +0200

    :construction: Added Vehicles migration with constraints :construction:

---
+++ database/migrations/2023_01_19_105706_create_vehicles_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVehiclesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->id();
+            $table->string('license_plate_number')->unique();
+            $table->string('vehicle_make');
+            $table->string('vehicle_model');
+            $table->bigInteger('vehicle_year');
+            $table->boolean('insured');
+            $table->date('date_of_last_service')->nullable();
+            $table->bigInteger('passenger_capacity');
+            $table->string('driver_id_number');
+            $table->timestamps();
+
+            #<------------------- Foreign Key Constraints ------------------->
+            $table->foreign('driver_id_number')
+                ->references('id_number')
+                ->on('drivers')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehicles');
+    }
+}
