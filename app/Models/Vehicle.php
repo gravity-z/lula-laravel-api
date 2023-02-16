86ad5aceb73dd775795f0f19bf8c5492cd36b558
gravity-z <zikolo.sakhe@gmail.com>
2023-02-16 09:18:51 +0200

    :construction: Added Eloquent Builder import :construction:

--- app/Models/Vehicle.php
+++ app/Models/Vehicle.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Builder;
 
 class Vehicle extends Model
 {
@@ -49,5 +50,41 @@ class Vehicle extends Model
     public function driver(): BelongsTo
     {
         return $this->belongsTo(Driver::class, 'driver_id_number', 'id_number');
+    }
+
+    /**
+     * Scope a query to only include vehicles of a given make.
+     *
+     * @param Builder $query
+     * @param string $make
+     * @return Builder
+     */
+    public function scopeVehicleMake(Builder $query, string $make): Builder
+    {
+        return $query->where('vehicle_make', $make);
+    }
+
+    /**
+     * Scope a query to only include vehicles where the date of last service is less than or equal to the date provided.
+     *
+     * @param Builder $query
+     * @param string $date
+     * @return Builder
+     */
+    public function scopeServiceDate(Builder $query, string $date): Builder
+    {
+        return $query->where('date_of_last_service', '<=', $date);
+    }
+
+    /**
+     * Scope a query to only include vehicles of a given age/model_year.
+     *
+     * @param Builder $query
+     * @param int $age
+     * @return Builder
+     */
+    public function scopeModelAge(Builder $query, int $age): Builder
+    {
+        return $query->where('model_year', '=', now()->subYears($age)->year);
     }
 }
